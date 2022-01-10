74fb6c340429c585311f3efeb2661d0e80df2984
Florian <florian.levasseur@hotmail.fr>
2022-01-10 16:15:32 +0100

    Ajout couleur de chaque catégorie et création du tableau final

--- controllers/home-controller.php
+++ controllers/home-controller.php
@@ -16,11 +16,28 @@ $xml = simplexml_load_file($_SESSION['config']['fluxRss'][0]);
 $xml2 = simplexml_load_file($_SESSION['config']['fluxRss'][1]);
 $xml3 = simplexml_load_file($_SESSION['config']['fluxRss'][2]);
 
+$array_xml = [];
+
+for($i=0; $i < $_SESSION['config']['nbArticles'] / 3; $i++) {
+    $xml->channel->item[$i]->color = 'red';
+    $array_xml[] = $xml->channel->item[$i];
+    $xml2->channel->item[$i]->color = 'blue';
+    $array_xml[] = $xml2->channel->item[$i];
+    $xml3->channel->item[$i]->color = 'yellow';
+    $array_xml[] = $xml3->channel->item[$i];
+}
+
+// var_dump($array_xml);
+
+foreach ($array_xml as $key => $value) {
+    var_dump($value);
+}
+
 $firstArticle = $xml->channel->item;
 $secondArticle = $xml2->channel->item;
 $thirdArticle = $xml3->channel->item;
-var_dump($firstArticle);
 
 foreach($xml->channel->item as $value){
+    // var_dump($value);
     // echo $value->title;
 }
--- views/home.php
+++ views/home.php
@@ -57,6 +57,9 @@ require_once '../controllers/home-controller.php';
       <span class="visually-hidden">Next</span>
     </button>
   </div>
+  <div>
+    
+  </div>
 
   <script src="../assets/script/script.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
