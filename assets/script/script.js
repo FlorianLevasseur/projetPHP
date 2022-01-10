ecdc1ee34dc014434773d05ab79368c5691a4157
Florian <florian.levasseur@hotmail.fr>
2022-01-10 13:44:40 +0100

    Ajout dark mode basique via style.css et script.js

---
+++ assets/script/script.js
@@ -0,0 +1,17 @@
+let date = new Date(Date.now() + 86400000); //86400000ms = 1 jour
+date = date.toUTCString();
+
+document.getElementById("darkMode").addEventListener("click", () => {
+    if(!document.body.classList.contains("darkBackColor")){
+        document.body.classList.add("darkBackColor")
+        document.cookie = 'mode=dark; path=/; expires=' + date;
+    } else {
+        document.body.classList.remove("darkBackColor")
+        document.cookie = 'mode=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC';
+    }
+})
+
+if(document.cookie.includes("mode=dark")) {
+    document.body.classList.add("darkBackColor")
+    document.getElementById("darkMode").checked = true
+}
--- views/parameters.php
+++ views/parameters.php
@@ -5,23 +5,48 @@ require_once '../controllers/parameters-controller.php';
 <!doctype html>
 
 <html lang="fr">
-  <head>
 
-    <title>Paramètres</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="../assets/style/style.css">
+<head>
 
-  </head>
+  <title>Paramètres</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="../assets/style/style.css">
 
-  <body>
-      
-    <h1 class="red">Paramètres</h1>
+</head>
+
+<body>
+  <h1 class="red">Paramètres</h1>
+  <div class="form-check form-switch">
+    <input class="form-check-input" type="checkbox" role="switch" id="darkMode">
+    <label class="form-check-label" for="darkMode">Dark Mode</label>
+  </div>
+  <form method="POST">
+    <div>
+      <label for="nbArticles">Nombre d'articles affichés sur la page d'accueil :</label>
+      <select class="form-select" aria-label="Default select example">
+        <option value="1">6</option>
+        <option value="2">9</option>
+        <option value="3">12</option>
+      </select>
+    </div>
+    <div class="mt-3">
+      <p>Choix des sujets : </p>
+      <div>
+        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault1">
+        <label class="form-check-label" for="flexCheckDefault1">
+          Default checkbox
+        </label>
+      </div>
+      <input type="submit" name="submitParam" value="Envoyer">
+  </form>
 
 
-    <script src="../assets/script/script.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-  </body>
+
+  <script src="../assets/script/script.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</body>
+
 </html>
