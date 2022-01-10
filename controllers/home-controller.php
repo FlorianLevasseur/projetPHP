43582b8928e55eee2654c2ebffc7feebf363c79b
Florian <florian.levasseur@hotmail.fr>
2022-01-10 15:28:12 +0100

    Suppression lignes inutiles

--- controllers/home-controller.php
+++ controllers/home-controller.php
@@ -12,14 +12,6 @@ if (!isset($_SESSION['config'])) {
     ];
 }
 
-$fluxRss = [
-    'News' => 'https://www.jeuxactu.com/rss/news.rss',
-    'Tests' => 'https://www.jeuxactu.com/rss/tests.rss',
-    'Ps5' => 'https://www.jeuxactu.com/rss/ps5.rss',
-    'Switch' =>'https://www.jeuxactu.com/rss/switch.rss',
-    'Xbox-Series-X' =>'https://www.jeuxactu.com/rss/xbox-series-x.rss'
-];
-
 $xml = simplexml_load_file($_SESSION['config']['fluxRss'][0]);
 $xml2 = simplexml_load_file($_SESSION['config']['fluxRss'][1]);
 $xml3 = simplexml_load_file($_SESSION['config']['fluxRss'][2]);
