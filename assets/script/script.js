77bde4997b6c749414628aa8c425159d916f009e
Florian <florian.levasseur@hotmail.fr>
2022-01-13 11:54:04 +0100

    Factorisation script

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,6 @@
 let date = new Date(Date.now() + 86400000); //86400000ms = 1 jour
 date = date.toUTCString();
+let arrayCard = document.getElementsByClassName("card")
 
 if (document.getElementById("infoModal") != null) {
     infoModal.addEventListener('click', (e) => {
@@ -13,140 +14,33 @@ if (document.getElementById("infoModal") != null) {
 }
 
 if (document.getElementById("darkMode") != null) {
-    if (document.getElementById("cardNews").children[0].checked) {
-        document.getElementById("cardNews").children[1].classList.add("text-danger")
-        document.getElementById("cardNews").children[1].classList.add("fw-bold")
-        document.getElementById("cardNews").classList.replace("border-white", "border-danger")
-        document.getElementById("cardNews").classList.replace("border-dark", "border-danger")
-    }
-
-    if (document.getElementById("cardTests").children[0].checked) {
-        document.getElementById("cardTests").children[1].classList.add("text-danger")
-        document.getElementById("cardTests").children[1].classList.add("fw-bold")
-        document.getElementById("cardTests").classList.replace("border-white", "border-danger")
-        document.getElementById("cardTests").classList.replace("border-dark", "border-danger")
-    }
-
-    if (document.getElementById("cardMulti").children[0].checked) {
-        document.getElementById("cardMulti").children[1].classList.add("text-danger")
-        document.getElementById("cardMulti").children[1].classList.add("fw-bold")
-        document.getElementById("cardMulti").classList.replace("border-white", "border-danger")
-        document.getElementById("cardMulti").classList.replace("border-dark", "border-danger")
-    }
-
-    if (document.getElementById("cardAndroid").children[0].checked) {
-        document.getElementById("cardAndroid").children[1].classList.add("text-danger")
-        document.getElementById("cardAndroid").children[1].classList.add("fw-bold")
-        document.getElementById("cardAndroid").classList.replace("border-white", "border-danger")
-        document.getElementById("cardAndroid").classList.replace("border-dark", "border-danger")
-    }
-
-    if (document.getElementById("cardPC").children[0].checked) {
-        document.getElementById("cardPC").children[1].classList.add("text-danger")
-        document.getElementById("cardPC").children[1].classList.add("fw-bold")
-        document.getElementById("cardPC").classList.replace("border-white", "border-danger")
-        document.getElementById("cardPC").classList.replace("border-dark", "border-danger")
-    }
-
-    document.getElementById("cardNews").addEventListener("click", (e) => {
-        if (e.target.children[0].checked) {
-            e.target.children[0].checked = false
-            e.target.children[1].classList.remove("text-danger")
-            e.target.children[1].classList.remove("fw-bold")
-            if (document.cookie.includes("mode=dark")) {
-                e.target.classList.replace("border-danger", "border-white")
-            } else {
-                e.target.classList.replace("border-danger", "border-dark")
-            }
-        } else {
-            e.target.children[0].checked = true
-            e.target.children[1].classList.add("text-danger")
-            e.target.children[1].classList.add("fw-bold")
-            e.target.classList.replace("border-white", "border-danger")
-            e.target.classList.replace("border-dark", "border-danger")
+    for(let elt of arrayCard){
+        if (elt.children[0].checked) {
+            elt.children[1].classList.add("text-danger")
+            elt.children[1].classList.add("fw-bold")
+            elt.classList.replace("border-white", "border-danger")
+            elt.classList.replace("border-dark", "border-danger")
         }
 
-    })
-
-    document.getElementById("cardTests").addEventListener("click", (e) => {
-        if (e.target.children[0].checked) {
-            e.target.children[0].checked = false
-            e.target.children[1].classList.remove("text-danger")
-            e.target.children[1].classList.remove("fw-bold")
-            if (document.cookie.includes("mode=dark")) {
-                e.target.classList.replace("border-danger", "border-white")
+        elt.addEventListener("click", (e) => {
+            if (e.target.children[0].checked) {
+                e.target.children[0].checked = false
+                e.target.children[1].classList.remove("text-danger")
+                e.target.children[1].classList.remove("fw-bold")
+                if (document.cookie.includes("mode=dark")) {
+                    e.target.classList.replace("border-danger", "border-white")
+                } else {
+                    e.target.classList.replace("border-danger", "border-dark")
+                }
             } else {
-                e.target.classList.replace("border-danger", "border-dark")
+                e.target.children[0].checked = true
+                e.target.children[1].classList.add("text-danger")
+                e.target.children[1].classList.add("fw-bold")
+                e.target.classList.replace("border-white", "border-danger")
+                e.target.classList.replace("border-dark", "border-danger")
             }
-        } else {
-            e.target.children[0].checked = true
-            e.target.children[1].classList.add("text-danger")
-            e.target.children[1].classList.add("fw-bold")
-            e.target.classList.replace("border-white", "border-danger")
-            e.target.classList.replace("border-dark", "border-danger")
-        }
-
-    })
-
-    document.getElementById("cardMulti").addEventListener("click", (e) => {
-        if (e.target.children[0].checked) {
-            e.target.children[0].checked = false
-            e.target.children[1].classList.remove("text-danger")
-            e.target.children[1].classList.remove("fw-bold")
-            if (document.cookie.includes("mode=dark")) {
-                e.target.classList.replace("border-danger", "border-white")
-            } else {
-                e.target.classList.replace("border-danger", "border-dark")
-            }
-        } else {
-            e.target.children[0].checked = true
-            e.target.children[1].classList.add("text-danger")
-            e.target.children[1].classList.add("fw-bold")
-            e.target.classList.replace("border-white", "border-danger")
-            e.target.classList.replace("border-dark", "border-danger")
-        }
-
-    })
-
-    document.getElementById("cardAndroid").addEventListener("click", (e) => {
-        if (e.target.children[0].checked) {
-            e.target.children[0].checked = false
-            e.target.children[1].classList.remove("text-danger")
-            e.target.children[1].classList.remove("fw-bold")
-            if (document.cookie.includes("mode=dark")) {
-                e.target.classList.replace("border-danger", "border-white")
-            } else {
-                e.target.classList.replace("border-danger", "border-dark")
-            }
-        } else {
-            e.target.children[0].checked = true
-            e.target.children[1].classList.add("text-danger")
-            e.target.children[1].classList.add("fw-bold")
-            e.target.classList.replace("border-white", "border-danger")
-            e.target.classList.replace("border-dark", "border-danger")
-        }
-
-    })
-
-    document.getElementById("cardPC").addEventListener("click", (e) => {
-        if (e.target.children[0].checked) {
-            e.target.children[0].checked = false
-            e.target.children[1].classList.remove("text-danger")
-            e.target.children[1].classList.remove("fw-bold")
-            if (document.cookie.includes("mode=dark")) {
-                e.target.classList.replace("border-danger", "border-white")
-            } else {
-                e.target.classList.replace("border-danger", "border-dark")
-            }
-        } else {
-            e.target.children[0].checked = true
-            e.target.children[1].classList.add("text-danger")
-            e.target.children[1].classList.add("fw-bold")
-            e.target.classList.replace("border-white", "border-danger")
-            e.target.classList.replace("border-dark", "border-danger")
-        }
-
-    })
+        })
+    }
 }
 
 if (document.cookie.includes("mode=dark")) {
@@ -166,7 +60,6 @@ if (document.cookie.includes("mode=dark")) {
         document.getElementById("darkMode").checked = true
     }
     let arrayCards = document.getElementsByClassName("card")
-    console.log(arrayCards)
     for (let item of arrayCards) {
         if(item.classList.contains("bg-white")){
             item.classList.replace("bg-white","bg-dark")
@@ -181,30 +74,18 @@ if (document.cookie.includes("mode=dark")) {
 if (document.getElementById("darkMode") != null) {
     document.getElementById("darkMode").addEventListener("click", () => {
         if (!document.body.classList.contains("darkBackColor")) {
-            document.getElementById("cardNews").classList.replace("bg-white", "bg-dark")
-            document.getElementById("cardTests").classList.replace("bg-white", "bg-dark")
-            document.getElementById("cardMulti").classList.replace("bg-white", "bg-dark")
-            document.getElementById("cardAndroid").classList.replace("bg-white", "bg-dark")
-            document.getElementById("cardPC").classList.replace("bg-white", "bg-dark")
-            document.getElementById("cardNews").classList.replace("border-dark", "border-white")
-            document.getElementById("cardTests").classList.replace("border-dark", "border-white")
-            document.getElementById("cardMulti").classList.replace("border-dark", "border-white")
-            document.getElementById("cardAndroid").classList.replace("border-dark", "border-white")
-            document.getElementById("cardPC").classList.replace("border-dark", "border-white")
+            for(let elt of arrayCard){
+                elt.classList.replace("bg-white", "bg-dark")
+                elt.classList.replace("border-dark", "border-white")
+            }
             document.body.classList.add("darkBackColor")
             document.cookie = 'mode=dark; path=/; expires=' + date;
         } else {
             document.body.classList.remove("darkBackColor")
-            document.getElementById("cardNews").classList.replace("bg-dark", "bg-white")
-            document.getElementById("cardTests").classList.replace("bg-dark", "bg-white")
-            document.getElementById("cardMulti").classList.replace("bg-dark", "bg-white")
-            document.getElementById("cardAndroid").classList.replace("bg-dark", "bg-white")
-            document.getElementById("cardPC").classList.replace("bg-dark", "bg-white")
-            document.getElementById("cardNews").classList.replace("border-white", "border-dark")
-            document.getElementById("cardTests").classList.replace("border-white", "border-dark")
-            document.getElementById("cardMulti").classList.replace("border-white", "border-dark")
-            document.getElementById("cardAndroid").classList.replace("border-white", "border-dark")
-            document.getElementById("cardPC").classList.replace("border-white", "border-dark")
+            for(let elt of arrayCard){
+                elt.classList.replace("bg-dark", "bg-white")
+                elt.classList.replace("border-white", "border-dark")
+            }
             document.cookie = 'mode=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC';
         }
     })
