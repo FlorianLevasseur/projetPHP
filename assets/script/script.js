db3c66f76432b518cb1a0657467c02d5e3771596
Florian <florian.levasseur@hotmail.fr>
2022-01-13 11:31:05 +0100

    Finalisation projet en rendant 'sexy' la page Paramètres

--- assets/script/script.js
+++ assets/script/script.js
@@ -12,15 +12,151 @@ if (document.getElementById("infoModal") != null) {
     })
 }
 
+if (document.getElementById("darkMode") != null) {
+    if (document.getElementById("cardNews").children[0].checked) {
+        document.getElementById("cardNews").children[1].classList.add("text-danger")
+        document.getElementById("cardNews").children[1].classList.add("fw-bold")
+        document.getElementById("cardNews").classList.replace("border-white", "border-danger")
+        document.getElementById("cardNews").classList.replace("border-dark", "border-danger")
+    }
+
+    if (document.getElementById("cardTests").children[0].checked) {
+        document.getElementById("cardTests").children[1].classList.add("text-danger")
+        document.getElementById("cardTests").children[1].classList.add("fw-bold")
+        document.getElementById("cardTests").classList.replace("border-white", "border-danger")
+        document.getElementById("cardTests").classList.replace("border-dark", "border-danger")
+    }
+
+    if (document.getElementById("cardMulti").children[0].checked) {
+        document.getElementById("cardMulti").children[1].classList.add("text-danger")
+        document.getElementById("cardMulti").children[1].classList.add("fw-bold")
+        document.getElementById("cardMulti").classList.replace("border-white", "border-danger")
+        document.getElementById("cardMulti").classList.replace("border-dark", "border-danger")
+    }
+
+    if (document.getElementById("cardAndroid").children[0].checked) {
+        document.getElementById("cardAndroid").children[1].classList.add("text-danger")
+        document.getElementById("cardAndroid").children[1].classList.add("fw-bold")
+        document.getElementById("cardAndroid").classList.replace("border-white", "border-danger")
+        document.getElementById("cardAndroid").classList.replace("border-dark", "border-danger")
+    }
+
+    if (document.getElementById("cardPC").children[0].checked) {
+        document.getElementById("cardPC").children[1].classList.add("text-danger")
+        document.getElementById("cardPC").children[1].classList.add("fw-bold")
+        document.getElementById("cardPC").classList.replace("border-white", "border-danger")
+        document.getElementById("cardPC").classList.replace("border-dark", "border-danger")
+    }
+
+    document.getElementById("cardNews").addEventListener("click", (e) => {
+        if (e.target.children[0].checked) {
+            e.target.children[0].checked = false
+            e.target.children[1].classList.remove("text-danger")
+            e.target.children[1].classList.remove("fw-bold")
+            if (document.cookie.includes("mode=dark")) {
+                e.target.classList.replace("border-danger", "border-white")
+            } else {
+                e.target.classList.replace("border-danger", "border-dark")
+            }
+        } else {
+            e.target.children[0].checked = true
+            e.target.children[1].classList.add("text-danger")
+            e.target.children[1].classList.add("fw-bold")
+            e.target.classList.replace("border-white", "border-danger")
+            e.target.classList.replace("border-dark", "border-danger")
+        }
+
+    })
+
+    document.getElementById("cardTests").addEventListener("click", (e) => {
+        if (e.target.children[0].checked) {
+            e.target.children[0].checked = false
+            e.target.children[1].classList.remove("text-danger")
+            e.target.children[1].classList.remove("fw-bold")
+            if (document.cookie.includes("mode=dark")) {
+                e.target.classList.replace("border-danger", "border-white")
+            } else {
+                e.target.classList.replace("border-danger", "border-dark")
+            }
+        } else {
+            e.target.children[0].checked = true
+            e.target.children[1].classList.add("text-danger")
+            e.target.children[1].classList.add("fw-bold")
+            e.target.classList.replace("border-white", "border-danger")
+            e.target.classList.replace("border-dark", "border-danger")
+        }
+
+    })
+
+    document.getElementById("cardMulti").addEventListener("click", (e) => {
+        if (e.target.children[0].checked) {
+            e.target.children[0].checked = false
+            e.target.children[1].classList.remove("text-danger")
+            e.target.children[1].classList.remove("fw-bold")
+            if (document.cookie.includes("mode=dark")) {
+                e.target.classList.replace("border-danger", "border-white")
+            } else {
+                e.target.classList.replace("border-danger", "border-dark")
+            }
+        } else {
+            e.target.children[0].checked = true
+            e.target.children[1].classList.add("text-danger")
+            e.target.children[1].classList.add("fw-bold")
+            e.target.classList.replace("border-white", "border-danger")
+            e.target.classList.replace("border-dark", "border-danger")
+        }
+
+    })
+
+    document.getElementById("cardAndroid").addEventListener("click", (e) => {
+        if (e.target.children[0].checked) {
+            e.target.children[0].checked = false
+            e.target.children[1].classList.remove("text-danger")
+            e.target.children[1].classList.remove("fw-bold")
+            if (document.cookie.includes("mode=dark")) {
+                e.target.classList.replace("border-danger", "border-white")
+            } else {
+                e.target.classList.replace("border-danger", "border-dark")
+            }
+        } else {
+            e.target.children[0].checked = true
+            e.target.children[1].classList.add("text-danger")
+            e.target.children[1].classList.add("fw-bold")
+            e.target.classList.replace("border-white", "border-danger")
+            e.target.classList.replace("border-dark", "border-danger")
+        }
+
+    })
+
+    document.getElementById("cardPC").addEventListener("click", (e) => {
+        if (e.target.children[0].checked) {
+            e.target.children[0].checked = false
+            e.target.children[1].classList.remove("text-danger")
+            e.target.children[1].classList.remove("fw-bold")
+            if (document.cookie.includes("mode=dark")) {
+                e.target.classList.replace("border-danger", "border-white")
+            } else {
+                e.target.classList.replace("border-danger", "border-dark")
+            }
+        } else {
+            e.target.children[0].checked = true
+            e.target.children[1].classList.add("text-danger")
+            e.target.children[1].classList.add("fw-bold")
+            e.target.classList.replace("border-white", "border-danger")
+            e.target.classList.replace("border-dark", "border-danger")
+        }
+
+    })
+}
 
 if (document.cookie.includes("mode=dark")) {
     document.body.classList.add("darkBackColor")
     if (document.getElementById("infoModal") != null) {
         document.getElementById("infoModal").classList.add("text-white")
-        document.getElementById("myTable").classList.replace("border-dark","border-white")
+        document.getElementById("myTable").classList.replace("border-dark", "border-white")
         document.getElementById("fullModal").classList.add("bg-dark")
-        document.getElementById("titleModal").classList.replace("text-dark","text-white")
-        document.getElementById("descriptionModal").classList.replace("text-dark","text-white")
+        document.getElementById("titleModal").classList.replace("text-dark", "text-white")
+        document.getElementById("descriptionModal").classList.replace("text-dark", "text-white")
     }
     if (document.getElementById("myNavbar") != null) {
         document.getElementById("myNavbar").classList.replace("navbar-light", "navbar-dark")
@@ -30,19 +166,45 @@ if (document.cookie.includes("mode=dark")) {
         document.getElementById("darkMode").checked = true
     }
     let arrayCards = document.getElementsByClassName("card")
-    for(let item of arrayCards){
-        item.classList.add("bg-dark")
+    console.log(arrayCards)
+    for (let item of arrayCards) {
+        if(item.classList.contains("bg-white")){
+            item.classList.replace("bg-white","bg-dark")
+        } else {
+            item.classList.add("bg-dark")
+        }
         item.classList.replace("border-dark", "border-white")
     }
+
 }
 
 if (document.getElementById("darkMode") != null) {
     document.getElementById("darkMode").addEventListener("click", () => {
         if (!document.body.classList.contains("darkBackColor")) {
+            document.getElementById("cardNews").classList.replace("bg-white", "bg-dark")
+            document.getElementById("cardTests").classList.replace("bg-white", "bg-dark")
+            document.getElementById("cardMulti").classList.replace("bg-white", "bg-dark")
+            document.getElementById("cardAndroid").classList.replace("bg-white", "bg-dark")
+            document.getElementById("cardPC").classList.replace("bg-white", "bg-dark")
+            document.getElementById("cardNews").classList.replace("border-dark", "border-white")
+            document.getElementById("cardTests").classList.replace("border-dark", "border-white")
+            document.getElementById("cardMulti").classList.replace("border-dark", "border-white")
+            document.getElementById("cardAndroid").classList.replace("border-dark", "border-white")
+            document.getElementById("cardPC").classList.replace("border-dark", "border-white")
             document.body.classList.add("darkBackColor")
             document.cookie = 'mode=dark; path=/; expires=' + date;
         } else {
             document.body.classList.remove("darkBackColor")
+            document.getElementById("cardNews").classList.replace("bg-dark", "bg-white")
+            document.getElementById("cardTests").classList.replace("bg-dark", "bg-white")
+            document.getElementById("cardMulti").classList.replace("bg-dark", "bg-white")
+            document.getElementById("cardAndroid").classList.replace("bg-dark", "bg-white")
+            document.getElementById("cardPC").classList.replace("bg-dark", "bg-white")
+            document.getElementById("cardNews").classList.replace("border-white", "border-dark")
+            document.getElementById("cardTests").classList.replace("border-white", "border-dark")
+            document.getElementById("cardMulti").classList.replace("border-white", "border-dark")
+            document.getElementById("cardAndroid").classList.replace("border-white", "border-dark")
+            document.getElementById("cardPC").classList.replace("border-white", "border-dark")
             document.cookie = 'mode=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC';
         }
     })
--- views/parameters.php
+++ views/parameters.php
@@ -17,36 +17,48 @@ require_once '../controllers/parameters-controller.php';
 </head>
 
 <body>
-
-  <h1 class="red">Paramètres</h1>
-
-  <span class="text-danger"><?= $error ?? '' ?></span>
-  
-  <div class="form-check form-switch">
-    <input class="form-check-input" type="checkbox" role="switch" id="darkMode">
-    <label class="form-check-label" for="darkMode">Dark Mode</label>
-  </div>
-  <form method="POST">
-    <div>
-      <label for="nbArticles">Nombre d'articles affichés sur la page d'accueil :</label>
-      <select class="form-select" aria-label="Default select example" name="article">
-        <option value="6" <?= $myConfig['nbArticles'] == 6 ? 'selected' : '' ?>>6</option>
-        <option value="9" <?= $myConfig['nbArticles'] == 9 ? 'selected' : '' ?>>9</option>
-        <option value="12" <?= $myConfig['nbArticles'] == 12 ? 'selected' : '' ?>>12</option>
-      </select>
+<div class="text-center">
+      <a href="/"><img class="img-fluid" src="../assets/img/Logo.png" alt="Logo du site"></a>
     </div>
-    <div class="mt-3">
-      <p>Choix des sujets : </p>
-      <?php foreach($fluxRss as $key => $value): ?>
+  <div class="container">
+   
+    <h1 class="text-center">Paramètres</h1>
+
+    <span class="text-danger"><?= $error ?? '' ?></span>
+
+    <div class="form-check form-switch">
+      <input class="form-check-input" type="checkbox" role="switch" id="darkMode">
+      <label class="form-check-label" for="darkMode">Dark Mode</label>
+    </div>
+    <form method="POST">
       <div>
-        <input class="form-check-input" type="checkbox" value="<?= $value ?>" id="flexCheckDefault1" name="checkbox[]" <?= in_array($value, $myConfig['fluxRss']) ? 'checked' : ''?>>
-        <label class="form-check-label" for="flexCheckDefault1">
-        <?= $key ?>
-        </label>
+        <label for="nbArticles">Nombre d'articles affichés sur la page d'accueil :</label>
+        <select class="form-select" aria-label="Default select example" name="article">
+          <option value="6" <?= $myConfig['nbArticles'] == 6 ? 'selected' : '' ?>>6</option>
+          <option value="9" <?= $myConfig['nbArticles'] == 9 ? 'selected' : '' ?>>9</option>
+          <option value="12" <?= $myConfig['nbArticles'] == 12 ? 'selected' : '' ?>>12</option>
+        </select>
       </div>
-        <?php endforeach; ?>
-      <input type="submit" name="submitParam" value="Envoyer">
-  </form>
+      <div class="mt-3">
+        <p>Choix des sujets : </p>
+        <div class="row m-0 p-0 justify-content-center">
+          <?php foreach ($fluxRss as $key => $value) : ?>
+            <div class="col-4">
+              <div class="card border border-dark text-center py-5 mb-3 bg-white" id="card<?= $key ?>">
+                <input class="form-check-input d-none" type="checkbox" value="<?= $value ?>" id="check<?= $key ?>" name="checkbox[]" <?= in_array($value, $myConfig['fluxRss']) ? 'checked' : '' ?>>
+                <label class="form-check-label" for="check<?= $key ?>">
+                  <?= $key ?>
+                </label>
+              </div>
+            </div>
+          <?php endforeach; ?>
+        </div>
+        <div class="text-center">
+          <input class="btn btn-danger" type="submit" name="submitParam" value="Envoyer">
+        </div>
+    </form>
+  </div>
+
 
 
 
